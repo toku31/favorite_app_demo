66060e361bf02213a9f4de40ff6391b69e30ae9e
user <palmtree8880@gmail.com>
2025-07-31 22:12:18 +0900

    トップ画面のタイトル

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ $totalPages = ceil($totalCount / $limit);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登録サイト一覧</title>
+    <title>お気に入りサイト一覧</title>
     <link rel="stylesheet" href="css/style.css?v=1.8">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
